da272bf296b131d822f735cb406d42c1274ee96e
ruifeng <ruifeng.yue@outlook.com>
2024-04-21 16:56:54 +0800

    feat(projects): restore state machine

--- projects/credit-card-processing/src/main/java/xyz/luckypeak/playground/creditcardprocessing/service/PaymentServiceImpl.java
+++ projects/credit-card-processing/src/main/java/xyz/luckypeak/playground/creditcardprocessing/service/PaymentServiceImpl.java
@@ -3,6 +3,7 @@ package xyz.luckypeak.playground.creditcardprocessing.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 import xyz.luckypeak.playground.creditcardprocessing.domain.Payment;
 import xyz.luckypeak.playground.creditcardprocessing.domain.PaymentEvent;
@@ -23,16 +24,35 @@ public class PaymentServiceImpl implements PaymentService {
 
   @Override
   public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
     return null;
   }
 
   @Override
   public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
     return null;
   }
 
   @Override
   public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+    StateMachine<PaymentState, PaymentEvent> stateMachine = build(paymentId);
     return null;
+  }
+
+  private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+    Payment payment = paymentRepository.getReferenceById(paymentId);
+    StateMachine<PaymentState, PaymentEvent> stateMachine =
+        stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+    stateMachine.stop();
+    stateMachine
+        .getStateMachineAccessor()
+        .doWithAllRegions(
+            sma -> {
+              sma.resetStateMachine(
+                  new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+            });
+    stateMachine.start();
+    return stateMachine;
   }
 }
