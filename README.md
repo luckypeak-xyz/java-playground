107a7fa184b56425860fd5f82956940d58c5efec
ruifeng <ruifeng.yue@outlook.com>
2024-04-30 21:45:22 +0800

    feat(projects): 使用 spring cloud api gateway 搭建 api gateway

--- README.md
+++ README.md
@@ -8,3 +8,5 @@
     - 介绍如何使用 spring state machine 实现处理信用卡状态转换的代码
 - simple-bank
 - order-service
+- simple-mall
+    - 服务发现 记得关掉全局代理
---
+++ projects/simple-mall/simple-mall-api-gateway/src/main/java/xyz/luckypeak/playground/SimpleMallApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package xyz.luckypeak.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMallApiGatewayApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleMallApiGatewayApplication.class, args);
+  }
+}
